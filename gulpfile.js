e7790238b9a127b748dec4d12a3464345e061926
Ilya Filippov <inwisloklem@Northpole-2.local>
2017-01-30 11:41:13 +0300

    Replace jade with pug

--- gulpfile.js
+++ gulpfile.js
@@ -2,11 +2,11 @@
 
 var gulp = require("gulp");
 
-var jade = require("gulp-jade");
 var less = require("gulp-less");
 var lessPluginGlob = require("less-plugin-glob");
 var plumber = require("gulp-plumber");
 var prettify = require("gulp-html-prettify");
+var pug = require("gulp-pug");
 var server = require("browser-sync").create();
 
 gulp.task("serve", ["markup", "style"], function() {
@@ -16,16 +16,16 @@ gulp.task("serve", ["markup", "style"], function() {
     ui: false
   });
 
-  gulp.watch("jade/**/*.jade", ["markup"]);
+  gulp.watch("pug/**/*.pug", ["markup"]);
   gulp.watch("less/**/*.less", ["style"]);
 
   gulp.watch("*.html").on("change", server.reload);
 });
 
 gulp.task("markup", function() {
-  gulp.src("jade/**/*.jade")
+  gulp.src("pug/**/*.pug")
     .pipe(plumber())
-    .pipe(jade())
+    .pipe(pug())
     .pipe(prettify({
       indent_char: ' ',
       indent_size: 2
